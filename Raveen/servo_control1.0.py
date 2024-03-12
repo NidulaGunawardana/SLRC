2ec2ae5fdb6a76b5e8b4200233155025f836901f
Raveen Pramuditha <raveenpspathirana99@gamil.com>
2024-03-12 19:46:07 +0530

    withoutGPIOZERO

--- Raveen/servo_control1.0.py
+++ Raveen/servo_control1.0.py
@@ -1,4 +1,4 @@
-from gpiozero import AngularServo
+'''from gpiozero import AngularServo
 from time import  sleep
 
 servo = AngularServo(19,min_pulse_width=0.0006,max_pulse_width=0.0023)
@@ -10,4 +10,19 @@ while (True):
     sleep(2)
     servo.angle = -90
     sleep(2)
-    
+    '''
+import RPi.GPIO as GPIO  
+from time import sleep   
+GPIO.setmode(GPIO.BOARD) 
+
+GPIO.setup(11,GPIO.OUT)  
+p = GPIO.PWM(19, 50)    
+p.start(0)               
+
+p.ChangeDutyCycle(3)    
+sleep(1)                
+p.ChangeDutyCycle(12)    
+sleep(1)
+
+p.stop()                 
+GPIO.cleanup() 
