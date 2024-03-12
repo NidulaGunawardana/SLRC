531395ec6382bbe5cff2d7261b83ce2e3f41dccb
Raveen Pramuditha <raveenpspathirana99@gamil.com>
2024-03-12 19:14:27 +0530

    servo1.0

---
+++ Raveen/servo_control1.0.py
@@ -0,0 +1,13 @@
+from gpiozero import AngularServo
+from time import  sleep
+
+servo = AngularServo(pin,min_pulse_width=0.0006,max_pulse_width=0.0023)
+
+while (True):
+    servo.angle = 90
+    sleep(2)
+    servo.angle = 0
+    sleep(2)
+    servo.angle = -90
+    sleep(2)
+    
