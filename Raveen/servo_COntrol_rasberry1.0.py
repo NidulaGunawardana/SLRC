604bb8653a5fd930dd6e656beec1bfd539c01282
Raveen Pramuditha <raveenpspathirana99@gamil.com>
2024-03-13 16:22:04 +0530

    remove while loop

--- Raveen/servo_COntrol_rasberry1.0.py
+++ Raveen/servo_COntrol_rasberry1.0.py
@@ -11,41 +11,25 @@ servo3 = AngularServo(26,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin
 servo4= AngularServo(17,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin_factory = factory)
 
 def servo_1_rotate(angle):
-    while (True):
-        for i in range(0,angle,1):
-            servo1.angle = i
-            sleep(0.01)
+    for i in range(0,angle,1):
+        servo1.angle = i
+        sleep(0.01)
         
-        for i in range(angle,0,-1):
-            servo1.angle = i
-            sleep(0.01)
 
 def servo_2_rotate(angle):
-    while (True):
-        for i in range(0,angle,1):
-            servo1.angle = i
-            sleep(0.01)
+    for i in range(0,angle,1):
+        servo1.angle = i
+        sleep(0.01)
         
-        for i in range(angle,0,-1):
-            servo1.angle = i
-            sleep(0.01)
 
 def servo_3_rotate(angle):
-    while (True):
-        for i in range(0,angle,1):
-            servo1.angle = i
-            sleep(0.01)
+    for i in range(0,angle,1):
+        servo1.angle = i
+        sleep(0.01)
         
-        for i in range(angle,0,-1):
-            servo1.angle = i
-            sleep(0.01)
-
 def servo_4_rotate(angle):
-    while (True):
-        for i in range(0,angle,1):
-            servo1.angle = i
-            sleep(0.01)
-        
-        for i in range(angle,0,-1):
-            servo1.angle = i
-            sleep(0.01)
+    for i in range(0,angle,1):
+        servo1.angle = i
+        sleep(0.01)
+
+servo_1_rotate(180)      
