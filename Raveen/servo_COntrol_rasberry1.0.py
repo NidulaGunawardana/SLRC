5532b91718e37cfd3af08f65d46fe64c262c0d05
Nidulagunawardana <nidulam@outlook.com>
2024-03-13 16:33:28 +0530

    Servo functions created

--- Raveen/servo_COntrol_rasberry1.0.py
+++ Raveen/servo_COntrol_rasberry1.0.py
@@ -11,16 +11,17 @@ servo3 = AngularServo(26,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin
 servo4= AngularServo(17,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin_factory = factory)
 
 def servo_1_rotate(angle):
-    servo1.angle = angle
+    servo1.angle = angle-90
         
 
 def servo_2_rotate(angle):
-    servo2.angle = angle
+    servo2.angle = angle-90
 
 def servo_3_rotate(angle):
-    servo3.angle = angle
+    servo3.angle = angle-90
         
 def servo_4_rotate(angle):
-    servo4.angle = angle
+    servo4.angle = angle-90
 
-servo_1_rotate(180)     
+servo_1_rotate(0)  
+servo_2_rotate(0)     
