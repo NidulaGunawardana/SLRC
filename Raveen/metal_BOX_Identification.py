b0f348cfc731ee84777aacb3571bde82dbe279f2
Raveen Pramuditha <raveenpspathirana99@gamil.com>
2024-03-19 21:16:42 +0530

    metal box detection 1.0

--- Raveen/metal_BOX_Identification.py
+++ Raveen/metal_BOX_Identification.py
@@ -1,17 +1,47 @@
-turnLeft(40)#turn left fromm the middle cicle
+import RPi.GPIO as GPIO
+from time import sleep
 
-goForward(40)
-
-distance_to_box = tof1Readings()
+pin_proxy = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin_proxy, GPIO.IN)
 
 def getBox(distance):
     if(distance<10):
-        
+        Arm()
 
-if(distance_to_box<10):
-    Stop
+def checkMetal():
+    return GPIO.input(pin_proxy)
+
+
+turnLeft(40)#turn left fromm the middle cicle
+goForward(40)#go forward
+
+distance_to_box = tof1Readings()
+metal = False  #detect metal
+
+##detect junction
+#goforward,right,left
+status = 1    #1 - forward,2 - right,3 - left
+while(metal):
+    if(status == 1):
+        goForward()
+        status += 1
+    elif(status == 2):
+        turnRight()
+        status += 1
+    else:
+        turnLeft()
+        status += 1
+    
+    if(distance_to_box<10):
+        Stop()
+        getBox(distance_to_box)
+        metal = checkMetal()
+    else:
+        goForward(40)
 else:
-    goForward(40)
+
 
 
 
