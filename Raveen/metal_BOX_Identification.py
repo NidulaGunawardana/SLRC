2a3ee2461be5492c0815f365fd138da0c8ec3e9d
Raveen Pramuditha <raveenpspathirana99@gamil.com>
2024-03-19 22:02:36 +0530

    box

--- Raveen/metal_BOX_Identification.py
+++ Raveen/metal_BOX_Identification.py
@@ -1,10 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-from motorRotating import goForward,goBackword,turnLeft,turnRight,Stop
-from  tofsensorreadings import tof1Readings
-from servo_COntrol_rasberry import Arm
-
 pin_proxy = 27
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -28,18 +24,31 @@ metal = False  #detect metal
 #goforward,right,left
 status = 1    #1 - forward,2 - right,3 - left
 
-while(True):
-    if(status == 1):
-        goForward()
-        status += 1
-    elif(status == 2):
-        turnRight()
-        status += 1
-    else:
-        turnLeft()
-        status += 1
+# while(True):
+#     if(status == 1):
+#         goForward()
+#         status += 1
+#     elif(status == 2):
+#         turnRight()
+#         status += 1
+#     else:
+#         turnLeft()
+#         status += 1
 
-    while(metal):
+#     while(metal):
+#         if(distance_to_box<10):
+#             Stop()
+#             getBox(distance_to_box)
+#             metal = checkMetal()
+#             if(metal == True):
+#                 break
+#         else:
+#             goForward(40)
+#     else:
+#         goBackword() #if not a metal go to the junction 
+#         #detect T junction
+
+while(metal):
         if(distance_to_box<10):
             Stop()
             getBox(distance_to_box)
@@ -48,9 +57,6 @@ while(True):
                 break
         else:
             goForward(40)
-    else:
-        goBackword() #if not a metal go to the junction 
-        #detect T junction
     
 
 
