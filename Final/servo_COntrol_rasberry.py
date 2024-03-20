667d5dc1f6a3a912b75de85865acd7f2498f9380
Nidula Gunawardana <nidulam@outlook.com>
2024-03-20 18:29:29 +0530

    Update function to handle edge cases final folder created

---
+++ Final/servo_COntrol_rasberry.py
@@ -0,0 +1,75 @@
+from gpiozero import AngularServo
+from time import  sleep
+
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory()
+
+servo1 = AngularServo(19,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin_factory = factory)
+servo2= AngularServo(21,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin_factory = factory)
+servo3 = AngularServo(26,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin_factory = factory)
+servo4= AngularServo(17,min_pulse_width = 0.0005,max_pulse_width = 2.5/1000,pin_factory = factory)
+
+def servo_1_rotate(angle):
+    servo1.angle = angle
+        
+
+def servo_2_rotate(angle):
+    servo2.angle = angle
+
+def servo_3_rotate(angle):
+    servo3.angle = angle
+        
+def servo_4_rotate(angle):
+    servo4.angle = angle-90
+
+# # servo_1_rotate(180)    
+# while True:
+#     servo_2_rotate(32)
+#     sleep(2)
+#     for i in range(-90,40,1):
+#         servo_1_rotate(i)
+#         print(i)
+#         sleep(0.01)
+            
+        
+#     for i in range(40,-90,-1):
+#         servo_1_rotate(i)
+#         print(i)
+#         sleep(0.01) 
+#     # servo_3_rotate(0)
+#     # sleep(2)
+
+
+#     servo_2_rotate(32)
+#     sleep(2)
+
+#     servo_2_rotate(35)
+#     sleep(2)
+
+#     servo_2_rotate(32)
+#     sleep(2)
+
+#     servo_2_rotate(29)
+#     sleep(1.8)
+#     servo_2_rotate(32)
+#     sleep(2)
+    
+#     servo_3_rotate(-20)
+    
+def Arm():
+    servo_2_rotate(32)
+    sleep(2)
+    for i in range(-90,25,1):
+         servo_1_rotate(i)
+         print(i)
+         sleep(0.01)
+    sleep(1)        
+        
+    # for i in range(40,-90,-1):
+    #      servo_1_rotate(i)
+    #      print(i)
+    #      sleep(0.01) 
+    # servo_3_rotate(0)
+    # sleep(2)
+# Arm()
