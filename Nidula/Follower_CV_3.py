0db1d593e304c9a030852adaf05922f86880b969
Nidula Gunawardana <nidulam@outlook.com>
2024-03-22 22:59:42 +0530

    Add left turn functionality and display blackline image

--- Nidula/Follower_CV_3.py
+++ Nidula/Follower_CV_3.py
@@ -34,8 +34,11 @@ while True:
 		cv2.putText(image,str(ang),(10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 		cv2.putText(image,str(error),(10, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 		cv2.line(image, (int(x_min),200 ), (int(x_min),250 ), (255,0,0),3)
+	if ang < -10:
+		print("left")
 	 
 	cv2.imshow("orginal with line", image)	
+	cv2.imshow("blackline", Blackline)
 	key = cv2.waitKey(1) & 0xFF	
 	if key == ord("q"):
 		break
