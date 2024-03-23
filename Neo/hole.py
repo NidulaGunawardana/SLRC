7e125cc3cabbe12316346fdf70a0a06fccfe82a9
neocodered <nadilhansith@gmail.com>
2024-03-23 15:40:34 +0530

    hole drop

---
+++ Neo/hole.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
+
+
+def capture_hole(video_capture):
+
+    ret, frame = video_capture.read()
+    frame = cv2.flip(frame,0)
+    frame = cv2.flip(frame,1)
+    width = int(640)
+    height = int(480)
+    
+    dimensions = (width,height)
+    frame = cv2.resize(frame,dimensions,interpolation=cv2.INTER_AREA)
+
+    green = [0, 255, 0]  # green in BGR colorspace
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit_green, upperLimit_green = get_limits(green)
+
+    mask_green = cv2.inRange(hsvImage, lowerLimit_green, upperLimit_green)
+
+    mask_green_ = Image.fromarray(mask_green)
+
+    bbox_green = mask_green_.getbbox()
+
+    if bbox_green is not None:
+        x1, y1, x2, y2 = bbox_green
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    else:
+        return None
+
+    if y2 < 440:
+        return "hole"
+
