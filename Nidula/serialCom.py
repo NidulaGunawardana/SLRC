5303ae35bfe80c715a0910a808a7a1a95029338a
Nidula Gunawardana <94860959+NidulaGunawardana@users.noreply.github.com>
2024-03-27 14:45:01 +0530

    Led functions added

--- Nidula/serialCom.py
+++ Nidula/serialCom.py
@@ -3,28 +3,44 @@ import serial
 import time
 
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 ser.reset_input_buffer()
 
-def greenLed():
-    ser.write(b"Green\n")
-    line = ser.readline().decode('utf-8').rstrip()
-    # print(line)
-    time.sleep(1)
-    # time.sleep(1)
+
+def cylinderLed():
+    ser.write(b"Cylinder\n")
+    line = ser.readline().decode("utf-8").rstrip()
+
+
+def boxLed():
+    ser.write(b"Box\n")
+    line = ser.readline().decode("utf-8").rstrip()
+
+
+def highLed():
+    ser.write(b"High\n")
+    line = ser.readline().decode("utf-8").rstrip()
+
+
+def midLed():
+    ser.write(b"Mid\n")
+    line = ser.readline().decode("utf-8").rstrip()
+
+
+def lowLed():
+    ser.write(b"Low\n")
+    line = ser.readline().decode("utf-8").rstrip()
     
-def blueLed():
-    ser.write(b"Blue\n")
-    line = ser.readline().decode('utf-8').rstrip()
-    # print(line)
-    time.sleep(1)
-    # time.sleep(1)
+
+def offLed():
+    ser.write(b"Off\n")
+    line = ser.readline().decode("utf-8").rstrip()
+
 
 while True:
-    greenLed()
-    blueLed()
-    # time.sleep(1)
-    # time.sleep(1)
-
-
-    
+    cylinderLed()
+    boxLed()
+    midLed()
+    highLed()
+    lowLed()
+    offLed()
