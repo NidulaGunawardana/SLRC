b411de6291820c84204f0ab8159e59e83fda796d
Nidula Gunawardana <94860959+NidulaGunawardana@users.noreply.github.com>
2024-03-27 20:49:07 +0530

    Add support for a second TOF sensor and improve timing

--- Raveen/tofsensorreadings.py
+++ Raveen/tofsensorreadings.py
@@ -3,19 +3,27 @@ import VL53L0X
 
 # Create a VL53L0X object for device on TCA9548A bus 1
 tof1 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
-
-
 tof1.open()
+tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+tof3 = VL53L0X.VL53L0X(tca9548a_num=0, tca9548a_addr=0x70)
+tof3.open()
+tof3.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
 
 
 # Start ranging on TCA9548A bus 1
-tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+
 
 timing1 = tof1.get_timing()
+timing2 = tof3.get_timing()
 
 if timing1 < 20000:
     timing1 = 20000
+    
+if timing2 < 20000:
+    timing2 = 20000
 
 def tof1Readings():
     global tof1
@@ -28,12 +36,23 @@ def tof1Readings():
   
     return distance1,tof1
 
+def tof3Readings():
+    # global tof3
+        # Get distance from VL53L0X on TCA9548A bus 0
+    distance2 = tof3.get_distance()
+    if distance2 > 0:
+        distance2 = distance2
+    else:
+        distance2 = 0
+      
+    return distance2, tof3
+
 # x = 0
 # # tof = 0
 # while(True):
-#     read1,tof = tof1Readings()
+#     read1,tof = tof3Readings()
 #     print(read1)
-#     # time.sleep(timing1/10000000.00)
+#     time.sleep(timing2/10000000.00)
 #     x+=1
     
 # tof.stop_ranging()
